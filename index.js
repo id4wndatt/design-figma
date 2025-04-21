5f643ee64167e92416dfcd44d045a0868d0d697e
Đàm Như Đạt <155456292+id4wndatt@users.noreply.github.com>
2025-04-21 15:57:25 +0700

    Cập nhật reponsive-tablet

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
     const chatWrapper = document.getElementById("chatWrapper");
-    const chatToggleBtn = document.getElementById("chatToggle");4
-
+    const chatToggleBtn = document.getElementById("chatToggle");
     let isChatOpen = false;
 
-    chatToggleBtn.addEventListener("click", function () {
-        isChatOpen = !isChatOpen;
-        chatWrapper.style.display = isChatOpen ? "flex" : "none";
-        chatToggleBtn.style.right = isChatOpen ? "570px" : "20px";
-        chatWrapper.style.top = isChatOpen ? "60px" : "20px";
+    if (chatToggleBtn) {
+        chatToggleBtn.addEventListener("click", function () {
+            isChatOpen = !isChatOpen;
 
-    });
+            chatWrapper.style.display = isChatOpen ? "flex" : "none";
+
+            const isTablet = window.innerWidth <= 768;
+
+            if (isTablet) {
+                chatToggleBtn.style.right = isChatOpen ? "420px" : "20px";
+                chatWrapper.style.top = isChatOpen ? "60px" : "20px";
+                chatWrapper.style.bottom = "auto";
+            } else {
+                chatToggleBtn.style.right = isChatOpen ? "570px" : "20px";
+                chatWrapper.style.top = isChatOpen ? "60px" : "20px";
+                chatWrapper.style.bottom = "auto";
+            }
+        });
+    }
 });
